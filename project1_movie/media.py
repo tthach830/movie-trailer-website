e5d115548f797bcc0444d8605930fba2d0f4af34
tthach830 <tthach830@users.noreply.github.com>
2015-04-23 21:30:51 -0700

    Committed by Udacity

---
+++ project1_movie/media.py
@@ -0,0 +1,14 @@
+import webbrowser
+class Movie():
+    def __init__(self, movie_title, movie_director, movie_writer, movie_starring, movie_storyline, poster_image,
+               trailer_youtube):
+        self.title = movie_title
+        self.director = movie_director
+        self.writer = movie_writer
+        self.starring = movie_starring
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
+        
